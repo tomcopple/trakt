1b02f06cc583282ffc93768861590a5e2e38096a
Tom Copple <tomcopple@gmail.com>
2020-12-17 20:43:11 +0000

    Finished it, seems to work

--- 2020-12 syncPlex.R
+++ 2020-12 syncPlex.R
@@ -15,27 +15,51 @@ trakt
 # Get Plex History --------------------------------------------------------
 token <- 'ABhPTJsJFC1CsCPKzzhb'
 
-plexTV <- content(GET("http://192.168.1.99:32400/library/sections/2/search?type=4", 
+plex <- content(GET("http://192.168.1.99:32400/library/sections/2/search?type=4", 
                       add_headers("X-Plex-Token" = token))) %>% 
     magrittr::extract2('MediaContainer') %>% 
     magrittr::extract2('Metadata') %>% 
-
-
     map_df(function(x) {
         as_tibble(x) %>% 
             select(any_of(c('ratingKey', 'title', 'grandparentTitle', 'parentTitle',
-                            'index', 'parentIndex')))
+                            'index', 'parentIndex', 'viewCount')))
     }) %>% 
     select(show = grandparentTitle, seasonName = parentTitle, title, 
-           ratingKey, season = parentIndex, episode = index)
+           ratingKey, season = parentIndex, episode = index, viewCount) %>% 
+    mutate(viewCount = ifelse(is.na(viewCount), FALSE, TRUE))
 
 
 # Join together to get Played ---------------------------------------------
 
-matched <- inner_join(trakt, plexTV)
+matched <- inner_join(trakt, plex, 
+                      by = c('show', 'season', 'episode')) %>% 
+    distinct(show, season, episode, seasonName, ratingKey, viewCount)
 glimpse(matched)
 
+unplayed <- matched %>% filter(!viewCount)
+glimpse(unplayed)
 
 # Scrobble to Plex --------------------------------------------------------
 
+issues <- tibble(ratingKey = "")
 
+markPlayed <- function(ratingKey) {
+ req <- httr::GET('http://192.168.1.99:32400/:/scrobble',
+                  query = list(
+                      "X-Plex-Token" = token,
+                      "identifier"   = "com.plexapp.plugins.library",
+                      "key"          = ratingKey
+                  ))   
+ 
+ if(status_code(req) != 200) {
+     print(str_c("Problem with ", ratingKey))
+     .GlobalEnv$issues <- rbind(issues, tibble(
+         ratingKey = ratingKey
+     ))
+ } else if (status_code(req) == 200) {
+     print(str_c(ratingKey, " successful"))
+ }
+ 
+}
+
+purrr::walk(.x = rev(unplayed$ratingKey), .f = markPlayed)
