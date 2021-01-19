7f50b28a05cfa1461ad4329ba029b6f169ff11f3
tomcopple <tomcopple@gmail.com>
2021-01-19 17:01:58 +0000

    Fixing chart

--- 2020-12 Letterboxd.R
+++ 2020-12 Letterboxd.R
@@ -5,8 +5,8 @@ library(tidyverse);library(xml2);library(lubridate)
 letFeed <- "https://letterboxd.com/tomcopple2/rss/"
 let <- httr::GET(letFeed) %>% 
     xml2::read_xml() %>% 
-    xml2::xml_find_all(xpath = 'channel') %>% 
-    xml2::xml_find_all(xpath = 'item') %>% 
+    xml2::xml_find_all(x = ., xpath = 'channel') %>% 
+    xml2::xml_find_all(x = ., xpath = 'item') %>% 
     xml2::as_list() %>% 
     map(unlist) %>% 
     map_df(bind_rows) %>% 
@@ -28,16 +28,20 @@ let <- bind_rows(let, letHist) %>%
     distinct(title, date, rating) %>% 
     mutate(year = year(date))
 
+let %>% arrange(desc(date))
+
 let %>% group_by(year) %>% count()
 
 let %>% filter(year == 2020) %>% arrange(desc(rating))
 
 let %>% arrange(date) %>% 
+    group_by(date) %>% summarise(count = n(), .groups = 'drop') %>% 
     full_join(data.frame(date = seq.Date(from = min(let$date), to = today(),
                               by = 'days'))) %>% 
+    mutate(count = replace_na(count, 0)) %>% 
     arrange(date) %>% 
-    mutate(count = ifelse(is.na(title), 0, 1)) %>% 
     mutate(n = c(cumsum(count[1:364]), 
                  zoo::rollsum(x = count, k = 365, align = 'right'))) %>% 
+    # filter(date >= "2017-01-01") %>% 
     ggplot(aes(x = date, y = n)) + geom_col()
 
