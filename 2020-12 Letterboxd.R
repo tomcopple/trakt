368fda71acd62e358c96ca25e39ac19c7769145e
Tom Copple <tomcopple@tomcopple.local>
2021-01-22 17:45:33 +0000

    Adding diary csv

--- 2020-12 Letterboxd.R
+++ 2020-12 Letterboxd.R
@@ -3,7 +3,7 @@
 library(tidyverse);library(xml2);library(lubridate)
 
 letFeed <- "https://letterboxd.com/tomcopple2/rss/"
-let <- httr::GET(letFeed) %>% 
+letNew <- httr::GET(letFeed) %>% 
     xml2::read_xml() %>% 
     xml2::xml_find_all(xpath = 'channel') %>% 
     xml2::xml_find_all(xpath = 'item') %>% 
@@ -13,24 +13,28 @@ let <- httr::GET(letFeed) %>%
     select(title = filmTitle, date = watchedDate, rating = memberRating) %>% 
     mutate(date = lubridate::ymd(date),
            rating = as.numeric(rating))
-glimpse(let)
 
-dropbox <- readRDS('traktShiny/dropbox.rds')
-letHist <- rdrop2::drop_read_csv(file = 'R/trakt/letterboxd/diary.csv',
-                                 dtoken = dropbox) %>% 
-    as_tibble() %>% 
-    select(title = Name, date = Watched.Date, rating = Rating) %>% 
-    mutate(date = lubridate::ymd(date))
+dropbox <- readRDS('traktShiny/dropbox.RDS')
 
-glimpse(letHist)
+diary <- rdrop2::drop_read_csv(file = 'R/trakt/letterboxd/diary.csv',
+                                dtoken = dropbox) %>% 
+    select(date = Watched.Date, title = Name, rating = Rating, tags = Tags) %>% 
+    mutate(date = lubridate::ymd(date)) %>% 
+    as_tibble()
 
-let <- bind_rows(let, letHist) %>% 
-    distinct(title, date, rating) %>% 
-    mutate(year = year(date))
+# letHist <- rdrop2::drop_read_csv(file = 'R/trakt/letterboxd/letHist.csv',
+#                                  dtoken = dropbox) %>% 
+#     as_tibble() %>% 
+#     mutate(date = lubridate::ymd(date))
 
-let %>% group_by(year) %>% count()
 
-let %>% filter(year == 2020) %>% arrange(desc(rating))
+let <- bind_rows(letNew, diary) %>% distinct(title, date, rating) %>% 
+    na.omit()
+# write_csv(let, here::here('tempData', 'letHist.csv'))
+# rdrop2::drop_upload(file = here::here('tempData', 'letHist.csv'),
+#                     path = 'R/trakt/letterboxd')
+
+let %>% mutate(year = year(date)) %>% group_by(year) %>% count()
 
 let %>% arrange(date) %>% 
     full_join(data.frame(date = seq.Date(from = min(let$date), to = today(),
@@ -39,5 +43,38 @@ let %>% arrange(date) %>%
     mutate(count = ifelse(is.na(title), 0, 1)) %>% 
     mutate(n = c(cumsum(count[1:364]), 
                  zoo::rollsum(x = count, k = 365, align = 'right'))) %>% 
-    ggplot(aes(x = date, y = n)) + geom_col()
+    # distinct(date,)
+    ggplot(aes(x = date, y = n)) + geom_col(position = 'dodge')
+
+let %>% filter(year(date) == 2020) %>% mutate(rating = rating * 2) %>% count(rating) %>% 
+    ggplot(aes(x = rating, y = n)) + geom_col()
+
+let %>% 
+    ggplot(aes(x = date, y = rating, color = rating)) + geom_point()
+
+diary %>%
+    mutate(home = str_detect(tags, 'home'),
+           out = str_detect(tags, 'out|mv')) %>% 
+    filter(date >= "2012-01-01") %>% 
+    full_join(data.frame(date = seq.Date(from = lubridate::ymd("2012-01-01"), 
+                                         to = today(),
+                                         by = 'days')))  %>% 
+    arrange(date) %>% 
+    mutate(home = replace_na(home, 0),
+           out = replace_na(out, 0)) %>% 
+    group_by(date) %>% 
+    summarise(home = sum(home), out = sum(out), .groups = 'drop') %>%
+    mutate(nHome = c(cumsum(home[1:29]), 
+                 zoo::rollsum(x = home, k = 30, align = 'right')),
+           nOut = c(cumsum(out[1:29]),
+                    zoo::rollsum(x = out, k = 30, align = 'right'))) %>% 
+    select(-home, -out) %>% 
+    gather(-date, key = series, value = n) %>% 
+    ggplot(aes(x = date, y = n, group = series, color = series)) +
+    geom_point(alpha = 0.1, size = 0.5) + 
+    geom_smooth(se   = FALSE, size = 1)
+
+diary %>% 
+    filter(str_detect(tags, 'home')) %>% 
+    separate(tags, into = c('home', 'tag'), sep = ", ")
 
