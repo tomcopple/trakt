22af81b33141e756b951a416a20da68d2f148e41
tomcopple <tomcopple@gmail.com>
2026-01-26 20:54:32 +0000

    temporary trakt auth

--- traktShiny/getTraktMovies.R
+++ traktShiny/getTraktMovies.R
@@ -1,19 +1,9 @@
 ## Get trakt movies
 library(tidyverse);library(httr);library(jsonlite);library(lubridate);library(xml2)
 
-traktClient <- oauth_client(
-    id = Sys.getenv('TRAKTSHINY_ID'),
-    secret = Sys.getenv('TRAKTSHINY_SECRET'),
-    token_url = 'https://api.trakt.tv/oauth/token',
-    name = 'traktShiny'
-)
-
 req <- httr2::request(base_url = 'https://api.trakt.tv/users/tomcopple/history/movies?limit=100000') %>% 
-    req_oauth_auth_code(
-        client = traktClient,
-        auth_url = 'https://trakt.tv/oauth/authorize',
-        redirect_uri = 'http://localhost:43451/'
-    ) %>%
+    reqAuthTrakt() %>% 
+    # req_auth_bearer_token(accessCode) %>%
     req_headers(
         "trakt-api-key" = trakt_id,
         "Content-Type" = "application/json",
